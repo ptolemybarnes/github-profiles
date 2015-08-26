b56f81b821b66a3df53e8b97a0c4c727a13b9cb3
Ptolemy <ptolemybarnes@gmail.com>
2015-08-26 11:36:28 +0100

    Gulp task runs then destroys http server

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
-var gulp       = require('gulp');
-var protractor = require("gulp-protractor").protractor;
+var gulp          = require('gulp');
+var protractor    = require("gulp-protractor").protractor;
+var ecstatic      = require('ecstatic');
+var http          = require('http');
+var enableDestroy = require('server-destroy');
 
-gulp.task('protractor', function(cb) {
+var server;
+
+gulp.task('server', function(){
+  var port = 8080;
+  server = http.createServer(
+    ecstatic({ root: '.' })
+  ).listen(port);
+  enableDestroy(server);
+
+  console.log('Server live on ' + port);
+});
+
+gulp.task('protractor', ['server'], function(cb) {
   gulp.src(["./test/e2e/*.js"])
       .pipe(protractor({
           configFile: "test/e2e/conf.js",
           args: ['--baseUrl', 'http://127.0.0.1:8000']
       }))
       .on('error', function(e) { throw e })
+      .on('close', function() { server.destroy() })
 });
 
 
